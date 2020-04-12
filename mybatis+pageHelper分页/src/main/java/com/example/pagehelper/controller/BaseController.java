1d716edd85787406216f19fd6f3720047a4603f5
melodyshu <1321497435@qq.com>
2020-04-12 12:46:17 +0800

    添加表返回结果

---
+++ mybatis+pageHelper分页/src/main/java/com/example/pagehelper/controller/BaseController.java
@@ -0,0 +1,58 @@
+package com.example.pagehelper.controller;
+
+import com.example.pagehelper.page.PageDomain;
+import com.example.pagehelper.page.TableDataInfo;
+import com.example.pagehelper.page.TableSupport;
+import com.example.pagehelper.util.SqlUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.beans.PropertyEditorSupport;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * web层通用数据处理
+ * 
+ * @author huangzhimao
+ */
+public class BaseController
+{
+    protected final Logger logger = LoggerFactory.getLogger(BaseController.class);
+
+    /**
+     * 设置请求分页数据
+     */
+    protected void startPage()
+    {
+        PageDomain pageDomain = TableSupport.buildPageRequest();
+        Integer pageNum = pageDomain.getPageNum();
+        Integer pageSize = pageDomain.getPageSize();
+        if ((pageNum != null) && (pageSize != null))
+        {
+            String orderBy = SqlUtil.escapeOrderBySql(pageDomain.getOrderBy());
+            PageHelper.startPage(pageNum, pageSize, orderBy);
+        }
+    }
+
+    /**
+     * 响应请求分页数据
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    protected TableDataInfo getDataTable(List<?> list)
+    {
+        TableDataInfo rspData = new TableDataInfo();
+        rspData.setCode(0);
+        rspData.setRows(list);
+        rspData.setTotal(new PageInfo(list).getTotal());
+        return rspData;
+    }
+
+}
--- mybatis+pageHelper分页/src/main/java/com/example/pagehelper/controller/SysLoginController.java
+++ mybatis+pageHelper分页/src/main/java/com/example/pagehelper/controller/SysLoginController.java
@@ -3,6 +3,7 @@ package com.example.pagehelper.controller;
 import com.example.pagehelper.domain.SysLogininfor;
 import com.example.pagehelper.mapper.SysLogininforMapper;
 import com.example.pagehelper.page.PageDomain;
+import com.example.pagehelper.page.TableDataInfo;
 import com.example.pagehelper.page.TableSupport;
 import com.example.pagehelper.util.SqlUtil;
 import com.github.pagehelper.PageHelper;
@@ -18,34 +19,22 @@ import java.util.List;
 @RestController
 @ResponseBody
 @RequestMapping("/login")
-public class SysLoginController {
+public class SysLoginController extends BaseController{
 
     @Autowired
     private SysLogininforMapper logininforMapper;
 
     @GetMapping("/list")
     @ResponseBody
-    public List<SysLogininfor> list(SysLogininfor logininfor)
+    public TableDataInfo list(SysLogininfor logininfor)
     {
         startPage();
         List<SysLogininfor> list = logininforMapper.selectLogininforList(logininfor);
-        return list;
+        TableDataInfo dataTable = getDataTable(list);
+        return dataTable;
     }
 
-    /**
-     * 设置请求分页数据
-     */
-    private void startPage()
-    {
-        PageDomain pageDomain = TableSupport.buildPageRequest();
-        Integer pageNum = pageDomain.getPageNum();
-        Integer pageSize = pageDomain.getPageSize();
-        if ((pageNum != null) && (pageSize != null))
-        {
-            String orderBy = SqlUtil.escapeOrderBySql(pageDomain.getOrderBy());
-            PageHelper.startPage(pageNum, pageSize, orderBy);
-        }
-    }
+
 
 
 }
