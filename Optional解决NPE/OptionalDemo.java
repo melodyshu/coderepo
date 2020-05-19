ea0aecb0d53e47b0e536f3efd0b13f2d4aa83219
huangzhimao <huangzhimao@cestc.cn>
2020-05-19 15:37:33 +0800

    修改Optional注释

--- Optional解决NPE/OptionalDemo.java
+++ Optional解决NPE/OptionalDemo.java
@@ -6,6 +6,8 @@ import java.util.Optional;
  * Optional 类是一个可以为null的容器对象。如果值存在则isPresent()方法会返回true，调用get()方法会返回该对象。
  * Optional 是个容器：它可以保存类型T的值，或者仅仅保存null。Optional提供很多有用的方法，这样我们就不用显式进行空值检测。
  * Optional 类的引入很好的解决空指针异常。
+ * java8所提供的Optional类则在减少NullPointException的同时，也提升了代码的美观度。
+ * 我们需要明确的是，它并不是对null关键字的一种替代，而是对于null判定提供了一种更加优雅的实现，从而避免NullPointException。
  *
  */
 public class OptionalDemo
